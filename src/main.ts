81b090e3aa5918b1d162a78ad34810e287309180
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-03-12 00:31:22 -0300

    refactor: initializable project

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,15 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import RabbitmqServer from './rabbitmq-server';
 
-//PRODUTOR
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(3002);
-// }
-
-//CONSUMIDOR
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://admin:admin@172.20.0.3:5672'],
-      queue: 'email-subscribe',
-      // false = manual acknowledgement; true = automatic acknowledgment
-      noAck: true,
-      // Get one by one
-      prefetchCount: 1,
-    },
-  });
-  await app.listen();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT);
+
+  await RabbitmqServer.getInstance().start();
+  await RabbitmqServer.getInstance().consume('notification', (message) =>
+    console.log(message.content.toString()),
+  );
 }
 
 bootstrap();
